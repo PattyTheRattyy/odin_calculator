40060e81633592ce3bee4a20c6fc3dfe292391b4
PatrickFavret <110774860+PatrickFavret@users.noreply.github.com>
2024-12-27 16:39:05 -0500

    Achieve basic functionality for the use case of adding two numbers (very janky)

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function addOperation(num1, num2){
+    console.log("addOperation reached");
     return num1 + num2;
 }
 function subtractOperation(num1, num2){
@@ -11,9 +12,10 @@ function divideOperation(num1, num2){
     return num1 / num2;
 }
 
-function operate(operator, num1, num2){
+function operate(num1, operator, num2){
     switch (operator){
         case '+':
+            console.log("+ operate reached");
             return addOperation(num1, num2);
         case '-':
             return subtractOperation(num1, num2);
@@ -25,9 +27,11 @@ function operate(operator, num1, num2){
 }
 
 const calcBody = document.querySelector("#calculator");
-const display = document.querySelector(".display");
+const display = document.querySelector("#display");
+const buttons = document.querySelector("#buttons");
 
 function getDisplayContent() {
+    console.log(display.textContent);
     return display.textContent;
 }
 
@@ -43,15 +47,73 @@ function displayDigit(buttonID) {
     addDisplayContent(buttonID);
 }
 
-const clearButton = document.querySelector("#clear");
-clearButton.addEventListener("click", () => {
+function clear(){
     setDisplayContent("");
+}
+
+const clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", () => {
+    clear();
+});
+
+const maxSize = 3;
+let theBigThree = [];
+console.log(theBigThree);
+operatorWasLastEntry = false;
+
+const addButton = document.querySelector("#plus");
+addButton.addEventListener("click", () => {
+    if (getDisplayContent() != "" && !operatorWasLastEntry){
+        theBigThree.push(getDisplayContent())
+        theBigThree.push("+");
+        console.log("the big three: " + theBigThree)
+        operatorWasLastEntry = true;
+        console.log("operator was pressed: " + operatorWasLastEntry)
+    } else {
+        console.log("empty display");
+    }
+});
+const subtractButton = document.querySelector("#minus");
+subtractButton.addEventListener("click", () => {
+    setDisplayContent("-");
+});
+const multiplyButton = document.querySelector("#multiply");
+multiplyButton.addEventListener("click", () => {
+    setDisplayContent("*");
+});
+const divideButton = document.querySelector("#divide");
+divideButton.addEventListener("click", () => {
+    setDisplayContent("/");
+});
+
+
+
+
+const equalsButton = document.querySelector("#equals");
+equalsButton.addEventListener("click", () => {
+    console.log(theBigThree);
+    theBigThree.push(getDisplayContent());
+    console.log("the big three: " + theBigThree);
+    console.log("the big one1: " + theBigThree[0]);
+    console.log("the big two2: " + theBigThree[1]);
+    console.log("the big three3: " + theBigThree[2]);
+    
+    result = operate(parseInt(theBigThree[0]), theBigThree[1], parseInt(theBigThree[2]));
+    setDisplayContent(result);
+    console.log(result);
 });
 
 
 const digitButtons = document.querySelectorAll(".digits");
 digitButtons.forEach((button) => {
     button.addEventListener("click", () => {
-        displayDigit(button.id);
+        if (operatorWasLastEntry) {
+            clear();
+            displayDigit(button.id);
+            operatorWasLastEntry = false;
+        } else {
+            displayDigit(button.id);
+        }
+        
     });
 });
